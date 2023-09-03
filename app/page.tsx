eb2b1cac1e5e4ccedaf05e3a3caa8b8981171416
levblanc <levblanc@gmail.com>
2023-09-03 21:16:19 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Button } from '@nextui-org/react';
+
+export default function Home() {
+  return (
+    <Button color='primary' radius='sm'>
+      click me!
+    </Button>
+  );
+}
